3e2670816e6eb5abd67ab4891572e84259266329
verethragna01 <rtsiry@live.fr>
2016-06-28 00:51:59 +0300

    notes

--- data_notes.js
+++ data_notes.js
@@ -1,3 +1,8 @@
+this application is distributed in 2 ways:
+1. as an web application
+2. as an API and have lot of possibilities of reusability
+
+
 /*
 	Data temlate is used for basic formlation
 	if needed we can modify the structure and add new atributes
