3aff8b770e99f3b58f351c02f6a20a9c4634e132
verethragna01 <rtsiry@live.fr>
2016-06-27 20:24:59 +0300

    basics pages routes

--- server/routes/pages.js
+++ server/routes/pages.js
@@ -1,11 +1,22 @@
 var router = require('express').Router();
 
-var pageLogic = require('../logic/pagelogic.js')
+var pageGenerator = require('../tools/pageGenerator.js')
 
-router.get('/:corporation/public', pagelogic.get);
-router.get('/login', function function_name (argument) {
-	// body...
-})
+// public access
+// the login page
+router.get('/login', function (req, res, next) {
+	res.sender('./neutral/Login');
+});
 
+// the registration page
+router.get('/register', function (req, res, next){
+	res.sender('./neutral/Register');
+});
+
+// internal public page
+router.get('/:corporation/public/:pageName', Generator.publicPage);
+
+// internal private pages
+router.post('/:corporation/private/:user/:pageName', Generator.privatePage);
 
 module.exports = router;
