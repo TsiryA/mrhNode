6b57192280e4f4ebcd2066e75690dcfccbfe6d65
verethragna01 <rtsiry@live.fr>
2016-06-29 21:01:46 +0300

    CRUD

---
+++ server/src/tools/crud.js
@@ -0,0 +1,151 @@
+import mongodb from 'mongodb';
+import db from './db_logic.js';
+import es6Promise from 'es6-promise';
+
+const Promise = es6Promise.Promise;
+const ObjectID = mongodb.ObjectID;
+
+// ====================== CREATE =========================== //
+const create = {};
+// collection
+create.collection = (collectionName, data) => {
+  return new Promise( (resolve, reject) => {
+    db.collection(collectionName).save(data, (err, res) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(res);
+      }
+    });
+  });
+}
+
+// document
+create.collection = (collectionName, data) => {
+  return new Promise( (resolve, reject) => {
+    db.collection(collectionName).insert(data, (err, res) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(res);
+      }
+    });
+  });
+}
+
+// ===================== READ ============================= //
+const read = {}
+// all element in collection
+read.all = collectionName => {
+  return new Promise( (resolve, reject) => {
+    db.collection(collectionName).find().toArray((err, res) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(res);
+      }
+    });
+  });
+}
+
+// elements
+read.elements = (collectionName, eltSpec) => {
+  return new Promise( (resolve, reject) => {
+    db.collection(collectionName).find(eltSpec).toArray((err, res) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(res);
+      }
+    });
+  });
+}
+// ===================== UPDATE ============================ //
+const update = {}
+// overwritting data
+update.overwrite = (collectionName, eltSpec, eltUpdate) => {
+  return new Promise( (resolve, reject) => {
+    db.collection(collectionName).update(eltSpec, eltUpdate, (err, res) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(res);
+      }
+    });
+  });
+}
+
+// update data
+update.updates = (collectionName, eltSpec, eltUpdate) => {
+  return new Promise( (resolve, reject) => {
+    db.collection(collectionName).update(eltSpec, {
+        $set: eltUpdate
+      }, (err, res) => {
+        if(err){
+          reject(err);
+        }else{
+          resolve(res);
+        }
+    });
+  });
+}
+
+// add to set
+update.addToSet = (collectionName, eltSpec, eltUpdate) => {
+  return new Promise( (resolve, reject) => {
+    db.collection(collectionName).update(eltSpec, {
+        $addToSet: eltUpdate
+      }, (err, res) => {
+        if(err){
+          reject(err);
+        }else{
+          resolve(res);
+        }
+    });
+  });
+}
+
+// ======================= DELETE =================== //
+const delete = {}
+// document
+delete.doc = (collectionName, data) => {
+  return new Promise( (resolve, reject) => {
+    db.collection(collectionName).remove(data, (err, res) => {
+      if(err){
+        reject(err);
+      }else{
+        resolve(res);
+      }
+    });
+  });
+}
+
+// key and values
+delete.key = (collectionName, eltSpec, eltDelete) => {
+  return new Promise( (resolve, reject) => {
+    db.collection(collectionName).update(eltSpec, {
+        $unset: eltDelete
+      }, (err, res) => {
+        if(err){
+          reject(err);
+        }else{
+          resolve(res);
+        }
+    });
+  });
+}
+
+// array element
+delete.arrayElement =(collectionName, arraySpec, eltDelete) => {
+  return new Promise( (resolve, reject) => {
+    db.collection(collectionName).update(arraySpec, {
+        $pull: eltDelete
+      }, (err, res) => {
+        if(err){
+          reject(err);
+        }else{
+          resolve(res);
+        }
+    });
+  });
+}
